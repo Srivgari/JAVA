916a100564484c2e9ebe9c248eeebc1b4a5c4165
garima <srivgarima61@gmail.com>
2019-02-27 22:53:24 +0530

    new

--- src/com/collecttion/www/ArrayListExample.java
+++ src/com/collecttion/www/ArrayListExample.java
@@ -1,11 +1,12 @@
 package com.collecttion.www;
 
 import java.util.ArrayList;
-
+// arraylist
 public class ArrayListExample {
   
 	public static void main(String[] args) {
 		
+		@SuppressWarnings("rawtypes")
 		ArrayList<Comparable> l = new ArrayList<Comparable>();
 		l.add("A");
 		l.add(10);
